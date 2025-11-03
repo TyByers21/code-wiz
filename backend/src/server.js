ea7aabd4c819bb7c348790b3aff266739627eac0
Ty Byers <tybyers21@gmail.com>
2025-11-02 21:46:42 -0500

    Initial API frontend and backend setup

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "success from backend 123" });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "api is up and running" });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT}`);
+});
