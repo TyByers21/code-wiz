30c8a8fe873dec7fd6abbd5a59e96bab50ecf627
Ty Byers <tybyers21@gmail.com>
2025-11-02 22:21:17 -0500

    Final changes for Sevalla deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "success from backend 123" });
-});
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "api is up and running" });
+});
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+}
+
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.listen(ENV.PORT, () => {
